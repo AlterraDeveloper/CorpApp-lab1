c3dcd7b336d63a458c1d8335282318a84af411e6
Eugene Kiselev <eugene.kiselev@financesoft.kg>
2019-09-10 15:16:29 +0600

    initial commit

---
+++ CorpAppLab1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CorpAppLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPane = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridForDishes = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPane.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridForDishes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabPane
+            // 
+            this.tabPane.Controls.Add(this.tabPage1);
+            this.tabPane.Controls.Add(this.tabPage2);
+            this.tabPane.Controls.Add(this.tabPage3);
+            this.tabPane.Location = new System.Drawing.Point(13, 13);
+            this.tabPane.Name = "tabPane";
+            this.tabPane.SelectedIndex = 0;
+            this.tabPane.Size = new System.Drawing.Size(760, 518);
+            this.tabPane.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridForDishes);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(752, 492);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(752, 492);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(752, 492);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridForDishes
+            // 
+            this.dataGridForDishes.AllowUserToDeleteRows = false;
+            this.dataGridForDishes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridForDishes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridForDishes.Location = new System.Drawing.Point(7, 7);
+            this.dataGridForDishes.Name = "dataGridForDishes";
+            this.dataGridForDishes.ReadOnly = true;
+            this.dataGridForDishes.Size = new System.Drawing.Size(739, 479);
+            this.dataGridForDishes.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 543);
+            this.Controls.Add(this.tabPane);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPane.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridForDishes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabPane;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridForDishes;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ CorpAppLab1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.ConnectionUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorpAppLab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var connectionString = GetConnectionString(string.Empty);
+
+            using(SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand("select * from dbo.Dishes;", sqlConnection);
+
+                sqlConnection.Open();
+
+                var reader = cmd.ExecuteReader();
+
+                var dict = new Dictionary<string, string>();
+
+                while (reader.Read())
+                {
+                    dict[reader[0].ToString()] = reader[1].ToString();
+                }
+
+                dataGridForDishes.DataSource = (from row in dict select new { ID = row.Key, DishName = row.Value }).ToList();
+            }
+            
+
+        }
+
+        private string GetConnectionString(string supposedValue)
+        {
+            DataConnectionDialog connectionDialog =
+               new DataConnectionDialog { TopMost = true };
+            connectionDialog.DataSources.Add(DataSource.SqlDataSource);
+            connectionDialog.SelectedDataSource = DataSource.SqlDataSource;
+            connectionDialog.SelectedDataProvider = DataProvider.SqlDataProvider;
+
+            SqlConnectionStringBuilder connectionBuilder =
+               new SqlConnectionStringBuilder(supposedValue ?? string.Empty)
+               { MultipleActiveResultSets = true };
+
+            connectionDialog.ConnectionString = connectionBuilder.ToString();
+
+            if (DataConnectionDialog.Show(connectionDialog) != DialogResult.OK)
+                return null;
+
+            return connectionDialog.ConnectionString;
+        }
+
+    }
+}
